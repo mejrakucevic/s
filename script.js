be1f8a09fee3bf6f75a137b95e075d211b0dbf1a
mejrakucevic <mejrakucevic420@gmail.com>
2023-07-18 21:34:47 +0200

    .

---
+++ script.js
@@ -0,0 +1,33 @@
+'use strict'
+
+console.log(this);
+
+const calcAge = function(birthYear) {
+    console.log(2037 - birthYear)
+    console.log(this);
+}
+
+calcAge(1991);
+
+const calcAgeArrow = birthYear => {
+    console.log(2037 - birthYear)
+    console.log(this);
+}
+
+calcAge(1980);
+
+const jonas = {
+    year: 1991,
+    calcAge : function() {
+        console.log(this);
+    }
+};
+jonas.calcAge();
+
+const matilda = {
+    year: 2017,
+};
+
+matilda.calcAge = jonas.calcAge;
+
+const f = jonas.calcAge;
